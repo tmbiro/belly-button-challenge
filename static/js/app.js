ebcf089cdd4a18d5c3b59ee78e77ae9ed42c22ef
tmbiro <tifanibiro@Tifanis-MacBook-Pro.local>
2023-04-04 19:08:14 -0400

    Added data pull

--- static/js/app.js
+++ static/js/app.js
@@ -22,7 +22,6 @@ function init() {
     // Use the first sample from the list to build the initial plots
     var sample_1 = names[0];
     console.log(sample_1);
-    makechart(sample_1);
     getdata(sample_1);
   });
 }
@@ -33,65 +32,4 @@ init();
 // Fetch new data each time a new sample is selected
 function newchoice(sample_i) {
   getdata(sample_i);
-  makechart(sample_i);
-}
-
-// 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-
-// Create the bar chart function.
-function makechart(sample) {
-  // Use d3.json to load and retrieve the samples.json file 
-  d3.json(url).then((data) => {
-
-    // Create a variable that holds the first sample in the array.
-    let sample_1 = data.samples.filter(sample_i => sample_i.id == sample)[0];
-
-    // Create variables that hold the otu_ids, otu_labels, and sample_values.
-    let otu_ID = sample_1.otu_ids;
-    let otu_label = sample_1.otu_labels;
-    let s_values = sample_1.sample_values;
-
-    // Create the yticks for the bar chart.
-
-    let top_10 = 10; 
-
-    var yticks = (
-      otu_ID
-      .slice(0, top_10)
-      .map(val => 'OTU ' + val)
-      .reverse()
-      );
-
-    top_labels = (
-      otu_label
-      .slice(0, top_10)
-      .map(val => val)
-      .reverse());
-
-    top_values = (
-      s_values
-      .slice(0, top_10)
-      .map(val => val)
-      .reverse());
-
-    // Create the trace for the bar chart. 
-    var bar_data = [{
-      x: top_values,
-      y: yticks,
-      text: top_labels,
-      type: 'bar',
-      orientation: 'h'
-    }];
-
-    // Create the layout for the bar chart. 
-    var bar_layout = {
-      hovermode: 'closest',
-      title: '<b>Top 10 Bacteria Cultures Found</b>'
-    };
-
-    // Use Plotly to plot the data with the layout. 
-    Plotly.newPlot('bar', bar_data, bar_layout);
-
-    
-  });
 }
